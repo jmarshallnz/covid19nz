379d1644a717705bac94e0c30607d33aaddcfac4
Jonathan Marshall <jmarshall@never.you.mind>
2021-08-02 12:23:09 +1200

    tidy up the plot a bit, and no longer have Gender as a split as we don't use it (so can keep Unknown in)

--- dhb_age_gender.R
+++ dhb_age_gender.R
@@ -2,7 +2,9 @@ library(tidyverse)
 library(lubridate)
 library(readxl)
 library(Manu)
-library(DHBins) #<- JM hacked version: https://github.com/jmarshallnz/DHBins/tree/covid_dhbs
+# JM hacked version of DHBins: https://github.com/jmarshallnz/DHBins/tree/covid_dhbs
+# remotes::install_github('jmarshallnz/DHBins', ref="covid_dhbs")
+library(DHBins) 
 
 # baselines
 popn <- read_csv("data/Subnational population estimates (DHB, DHB constituency), by age and sex, at 30 June 1996-2020 (2020 boundaries)/TABLECODE7509_Data_21d61ee0-b582-400b-8deb-d973e38d64ac.csv")
@@ -25,7 +27,7 @@ popn_summary <- popn %>%
                             "Auckland Metro" = c("Auckland", "Waitemata", "Counties Manukau"),
                             "Hawkes Bay" = "Hawke's Bay",
                             "Capital & Coast and Hutt Valley" = c("Capital and Coast", "Hutt Valley"))) %>%
-  group_by(DHB, Age, Gender) %>%
+  group_by(DHB, Age) %>%
   summarise(Population = sum(Population)) %>%
   ungroup()
 
@@ -38,16 +40,20 @@ vacc_dhbs <- vacc %>% select(DHB = `DHB of residence`,
                  Vacc = `# doses administered`) %>%
   filter(DHB != "Overseas / Unknown",
          Age != "90+/Unknown",
-         Age != "90 + years / Unknown",
-         Gender != "Other / Unknown",
-         Gender != "Unknown/Other") %>%
-  group_by(DHB, Age, Gender, Dose) %>%
+         Age != "90 + years / Unknown") %>%
+#         Gender != "Other / Unknown",
+#         Gender != "Unknown/Other") %>%
+  group_by(DHB, Age, Dose) %>%
   summarise(Vacc = sum(Vacc)) %>%
   left_join(popn_summary) %>%
   group_by(DHB, Age, Dose) %>%
   summarise(Vacc = sum(Vacc),
             Population = sum(Population))
 
+# check counts:
+vacc_dhbs %>% group_by(Dose) %>%
+  summarise(sum(Vacc), sum(Population))
+  
 # combine age groups further, and munge
 # into format ready for DHBins
 vacc_dhbs %>%
@@ -59,12 +65,15 @@ vacc_dhbs %>%
   group_by(DHB, Age, Dose) %>%
   summarise(Vacc = sum(Vacc), Population=sum(Population)) %>%
   pivot_wider(names_from=Dose, values_from=Vacc, names_prefix="Dose") %>%
-  mutate(None = Population - Dose1,
-         One = Dose1 - Dose2,
-         Two = Dose2) %>%
-  select(DHB, Age, None:Two) %>%
+  mutate(Unvaccinated = Population - Dose1,
+         `One dose` = Dose1 - Dose2,
+         `Fully vaccinated` = Dose2) %>%
+  select(DHB, Age, Unvaccinated:`Fully vaccinated`) %>%
   ungroup() %>%
   unite(DHBAge, DHB, Age) -> vacc_counts
+
+# check counts
+vacc_counts %>% summarise(across(Unvaccinated:`Fully vaccinated`, sum))
 
 # Setup triangles
 tris <- tri_alloc(vacc_counts %>% select(-DHBAge),
@@ -77,7 +86,8 @@ tris_long <- tibble(DHB=rep(rownames(tris),6),
                     tri_id=rep(1:6,each=nrow(tris))) %>%
   separate(DHB, into=c("DHB", "Age"), sep="_") %>%
   mutate(DHB = fct_recode(DHB,
-                          "Wellington" = "Capital & Coast and Hutt Valley"))
+                          "Wellington" = "Capital & Coast and Hutt Valley")) %>%
+  mutate(Vacc = fct_relevel(Vacc, "Unvaccinated", "One dose"))
 
 png("dhb_by_age.png", width=1980, height=1080)
 ggplot(tris_long) +
