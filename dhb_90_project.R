f6578749d742b56c8153793e30d79f82d2192611
Jonathan Marshall <j.c.marshall@massey.ac.nz>
2021-09-22 18:18:12 +1200

    adds code for animated version of project 90 plot

--- dhb_90_project.R
+++ dhb_90_project.R
@@ -80,3 +80,87 @@ current_counts %>%
   coord_cartesian(clip="off")
 dev.off()
 
+# same thing, but animated...
+vacc_dat <- read_excel("data/210920_-_rate_ratios_and_uptake_over_time.xlsx",
+                       sheet=4) %>%
+  rename(Week = `Week ending date`,
+         Dose = `Dose number`,
+         Ethnicity = `Ethnic group`,
+         Age = `Age group`,
+         DHB = `DHB of residence`,
+         Vacc = `# doses administered`) %>%
+  filter(Dose != 0) %>%
+  filter(!(Age %in% c("0 to 4", "5 to 9", "Unknown"))) %>%
+  group_by(DHB, Week, Dose, Age) %>%
+  summarise(Vacc = sum(Vacc)) %>%
+  ungroup()
+
+popn_dat <- read_excel("data/210920_-_rate_ratios_and_uptake_over_time.xlsx",
+                       sheet=5) %>%
+  rename(Ethnicity = `Ethnic group`,
+         Age = `Age group`,
+         DHB = `DHB of residence`,
+         Population = `# people (HSU)`) %>%
+  group_by(DHB, Age) %>% summarise(Population = sum(Population)) %>%
+  ungroup()
+
+vacc_dat %>% count(Age)
+popn_dat %>% count(Age)
+
+# see what is missing
+vacc_dat %>% anti_join(popn_dat) %>% as.data.frame()
+
+# join up
+all_dat <- vacc_dat %>% ungroup() %>% complete(DHB, Week, Age, Dose, fill=list(Vacc=0)) %>%
+  left_join(popn_dat)
+
+# check:
+all_dat %>% group_by(Week, Dose) %>% summarise(Total = sum(Population, na.rm=TRUE)) %>%
+  pull(Total) %>% unique()
+
+# OK, now cumsum up the vacc counts
+ready_to_go <- all_dat %>% group_by(DHB, Age, Dose) %>%
+  arrange(Week) %>%
+  mutate(Vacc = cumsum(Vacc)) %>%
+  filter(Age != "< 12",
+         DHB != "Overseas and undefined") %>%
+  ungroup() %>%
+  mutate(Prop = Vacc/Population) %>%
+  mutate(Dose = fct_recode(as_factor(Dose), `One Dose` = "1", `Fully vaccinated` = "2")) %>%
+  mutate(Week = as_factor(format(Week, "%d %B %Y")))
+
+library(gganimate)
+#png("test.png", width=1280, height=720)
+anim <- ggplot(ready_to_go) +
+  geom_line(aes(x=Prop, y=fct_rev(DHB)), col="grey70", size=1.5) +
+  geom_segment(data=NULL, aes(x=0.9,y=0.5, xend=0.9,yend=20.5), size=1, col="grey30") +
+  geom_point(aes(x=Prop, y=fct_rev(DHB), shape=Age, fill=Prop >= 0.9), size=4) +
+  facet_wrap(vars(Dose)) +
+  scale_fill_manual(values = colours) +
+  scale_shape_manual(values = paste(c("circle", "square", "diamond", "triangle", "triangle down"), "filled")) +
+  scale_x_continuous(limits = c(0,1), expand=c(0,0), breaks=seq(0,1,by=0.2), labels = scales::label_percent()) +
+  labs(title = "Progress towards 90%",
+       subtitle = "Vaccination rates by DHB and Age at {current_frame}",
+       x = NULL,
+       y = NULL,
+       shape = NULL) +
+  annotate("text", x=0.9, y=21, label="90%", size=7) +
+  scale_y_discrete(expand=c(0.03,0,0.03,0)) +
+  guides(fill = 'none') +
+  theme_minimal(base_size=24) +
+  theme(panel.spacing.x = unit(72, "pt"),
+        legend.position = 'bottom',
+        plot.title = element_text(size=36),
+        legend.margin = margin(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.text.x = element_text(size=16, colour='grey70'),
+        plot.margin = margin(32, 32, 32, 32, "pt")) +
+  coord_cartesian(clip="off") +
+  transition_manual(Week)
+#dev.off()
+
+animate(anim, renderer = gifski_renderer(file="90_project_progress.gif", loop=TRUE),
+        width = 1280, height = 720, units = "px", duration=5 + length(unique(ready_to_go$Week)) / 5, end_pause=5)
+
+
