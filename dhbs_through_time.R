30dd138238769749d76d852c2b18f6cd0bf0da03
Jonathan Marshall <j.c.marshall@massey.ac.nz>
2021-10-22 15:25:33 +1300

    updates data and adds another visualiation for 90% targets

--- dhbs_through_time.R
+++ dhbs_through_time.R
@@ -11,8 +11,11 @@ vacc_dat <- read_excel("data/211017_-_cvip_equity_-_rate_ratios_and_uptake_over_
          DHB = `DHB of residence`,
          Vacc = `# doses administered`) %>%
   filter(Dose != 0) %>%
-  filter(!(Age %in% c("0 to 4", "5 to 9", "Unknown"))) %>%
-  group_by(Week, Dose, Age) %>%
+#  filter(!(Age %in% c("0 to 4", "5 to 9", "Unknown"))) %>%
+  filter(!(DHB %in% c("Overseas and undefined", "Unknown"))) %>%
+ # filter(DHB %in% c("Auckland", "Waitemata", "Counties Manukau"),
+#         Dose == 2) %>%
+  group_by(Week, Dose, DHB) %>%
   summarise(Vacc = sum(Vacc))
 
 popn_dat <- read_excel("data/211017_-_cvip_equity_-_rate_ratios_and_uptake_over_time.xlsx",
@@ -21,7 +24,8 @@ popn_dat <- read_excel("data/211017_-_cvip_equity_-_rate_ratios_and_uptake_over_
          Age = `Age group`,
          DHB = `DHB of residence`,
          Population = `# people (HSU)`) %>%
-  group_by(Age) %>% summarise(Population = sum(Population))
+#  filter(DHB %in% c("Auckland", "Waitemata", "Counties Manukau")) %>%
+  group_by(DHB) %>% summarise(Population = sum(Population))
 
 vacc_dat %>% count(Age)
 popn_dat %>% count(Age)
@@ -30,7 +34,7 @@ popn_dat %>% count(Age)
 vacc_dat %>% anti_join(popn_dat) %>% as.data.frame()
 
 # join up
-all_dat <- vacc_dat %>% ungroup() %>% complete(Week, Age, Dose, fill=list(Vacc=0)) %>%
+all_dat <- vacc_dat %>% ungroup() %>% complete(Week, Dose, DHB, fill=list(Vacc=0)) %>%
   left_join(popn_dat)
 
 # check:
@@ -41,30 +45,38 @@ all_dat %>% group_by(Week, Dose) %>% summarise(Total = sum(Population, na.rm=TRU
 #age_cats <- data.frame(cuts=seq(0,80,by=10),
 #                       labs=c("0 to 9", "10 to 19", "20 to 29", "30 to 39", "40 to 49",
 #                              "50 to 59", "60 to 69", "70 to 79", "80+"))
-weekly_vacc <- all_dat %>% filter(Age != "< 12") %>%
-  group_by(Week, Age, Dose) %>%
+weekly_vacc <- all_dat %>% #filter(Age == "12-19") %>%
+  group_by(Week, Dose, DHB) %>%
   summarise(Vacc = sum(Vacc), Population = sum(Population, na.rm=TRUE))
 
 # this is not cummaltive, so make it so
-cumm_vacc <- weekly_vacc %>% group_by(Age, Dose) %>%
+cumm_vacc <- weekly_vacc %>% group_by(Dose, DHB) %>%
   arrange(Week) %>%
-  mutate(Vacc = cumsum(Vacc),
-         Dose = paste("Dose", Dose)) %>%
-  mutate(Week = as.Date(Week))
-
-png("vacc_by_age_through_time.png", width=1980, height=1080)
+  mutate(Vacc = cumsum(Vacc)) %>%
+  mutate(Week = as.Date(Week),
+         Dose = paste("Dose", Dose),
+         OverTheLine = Vacc/Population >= 0.9)
+cumm_vacc %>% filter(OverTheLine)
+png("vacc_by_dhb.png", width=1980, height=1080)
 ggplot(cumm_vacc) +
-  geom_line(aes(x=Week, y=Vacc/Population*100, group=Age), col='black', size=4) +
-  geom_line(aes(x=Week, y=Vacc/Population*100, col=Age), size=3) +
-  facet_wrap(vars(Dose)) +
-  scale_color_viridis_d(direction = -1) +
-  scale_y_continuous(limits = c(0,100), expand=c(0,0), breaks=seq(0,100,by=20)) +
+  geom_hline(yintercept=90, size=1) +
+  geom_line(aes(x=Week, y=Vacc/Population*100, group=Dose), col='black', size=4) +
+  geom_line(aes(x=Week, y=Vacc/Population*100, col=Dose), size=3) +
+  facet_wrap(vars(DHB)) +
+  scale_color_viridis_d(direction = 1) +
+  scale_y_continuous(limits = c(0,100), breaks=90) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b") +
-  guides(colour = guide_legend(nrow = 1)) +
+  guides(colour = guide_legend(nrow = 1),
+         alpha = 'none') +
   theme_minimal(base_size=36) +
   labs(x = NULL,
-       y = "Doses per 100 people",
-       title = "COVID-19 Vaccination rates by Age: The race is on") +
+       y = NULL,
+       colour = NULL,
+       title = "Are we there yet? Progress to 90%") +
   theme(legend.position='bottom',
-        legend.key.width = unit(48, "pt"))
+        legend.key.width = unit(48, "pt"),
+        axis.text = element_text(size = 18),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(size=0.5),
+        panel.grid.minor = element_blank())
 dev.off()
